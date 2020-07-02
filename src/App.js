e106d5afdbdbf5dfd8f3659849661968811fb5f7
rednirug <sgurinderpreet@gmail.com>
2020-07-01 20:46:41 -0700

    Link updates

--- src/App.js
+++ src/App.js
@@ -4,16 +4,18 @@ import About from "./pages/About";
 import Contact from "./pages/Contact";
 import Landing from "./pages/Landing";
 import Nav from "./pages/Nav";
+import Resume from "./pages/Resume"
 import { BrowserRouter as Router, Route} from "react-router-dom"
 
 function App() {
   return (
     <div>
-      <Nav />
       <Router>
+        <Nav />
         <Route exact path="/" component={Landing} />
         <Route path="/contact" component={Contact} />
         <Route path="/about" component={About} />
+        <Route path="/pdf.html" component={Resume} />
       </Router>
     </div>
   );
--- src/pages/Resume/index.js
+++ src/pages/Resume/index.js
@@ -1,79 +1,62 @@
 import React from "react";
-import { Link } from "react-router-dom";
 
-function Nav() {
-	return (
-		<div id="top">
-        <section id="name">
+function Resume() {
+    return (
+        <div>
+            <section id="name">
                 <h1>Gurinderpreet Singh</h1>
         </section>
 
         <section id="links">
-            <Link to="/about">
+
             <div className="link">
                 <div id="about">
                     <div className="zero">
-                <li>About</li>
+                <a href="index.html">About</a>
                     </div>
                 </div>
             </div>
-            </Link>
 
-            <Link to="/">
             <div className="link">
                 <div id="portfoliolink">
                     <div className="zero">
-                <li>Portfolio</li>
+                <a href="portfolio.html">Portfolio</a>
                     </div>
                 </div>
             </div>
-            </Link>
 
-            <Link to="/contact">
             <div className="link">
                 <div id="contact">
                     <div className="zero">
-                <li>Contact</li>
+                <a href="contact.html">Contact</a>
                     </div>
                 </div>
             </div>
-            </Link>
-
-            <Link to="https://www.linkedin.com/in/gurinderpreet-singh-9363291ab/">
             <div className="link">
                 <div id="contact">
                     <div className="zero">
-                <li>LinkedIn</li>
+                <a href="https://www.linkedin.com/in/gurinderpreet-singh-9363291ab/">LinkedIn</a>
                     </div>
                 </div>
             </div>
-            </Link>
-
-            <Link to="https://github.com/Gurinderp">
             <div className="link">
                 <div id="contact">
                     <div className="zero">
-                <li>GitHub</li>
+                <a href="https://github.com/Gurinderp">GitHub</a>
                     </div>
                 </div>
             </div>
-            </Link>
-
-            <Link to="/pdf.html">
             <div className="link">
                 <div id="contact">
                     <div className="zero">
-                <li>Resume</li>
+                <a href="./pdf.html">Resume</a>
                     </div>
                 </div>
             </div>
-            </Link>
-
 
         </section>
-    </div>
-        
-	);
+        </div>
+    );
 }
 
-export default Nav;
+export default Resume;
